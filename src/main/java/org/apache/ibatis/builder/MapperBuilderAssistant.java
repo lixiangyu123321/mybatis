6a8deb0b0a86dca74250be9463b4069e3f2db9b7
11188813 <2809892243@qq.com>
2025-12-08 09:20:25 +0800

    remove test code

--- src/main/java/org/apache/ibatis/builder/MapperBuilderAssistant.java
+++ src/main/java/org/apache/ibatis/builder/MapperBuilderAssistant.java
@@ -412,7 +412,7 @@ public class MapperBuilderAssistant extends BaseBuilder {
       String databaseId,
       LanguageDriver lang,
       String resultSets) {
-    
+
     if (unresolvedCacheRef) {
       throw new IncompleteElementException("Cache-ref not yet resolved");
     }
@@ -420,6 +420,7 @@ public class MapperBuilderAssistant extends BaseBuilder {
     id = applyCurrentNamespace(id, false);
     boolean isSelect = sqlCommandType == SqlCommandType.SELECT;
 
+    // remove test code
     MappedStatement.Builder statementBuilder = new MappedStatement.Builder(configuration, id, sqlSource, sqlCommandType);
     statementBuilder.resource(resource);
     statementBuilder.fetchSize(fetchSize);
